904bf8e1fa2c622fbedfe56d21c50d9c3ccd9568
techgal14 <189483818+techgal14@users.noreply.github.com>
2025-03-05 00:57:22 -0800

    add HTML
    - add paragraph and list
    - change fonts
    - update CSS

--- src/interact.js
+++ src/interact.js
@@ -1,12 +1,27 @@
-function generatePositivity(event) {
-  event.preventDefault();
-
+function displayInspiration(response) {
   new Typewriter("#inspiration", {
-    strings: "whatever positive thing is generated",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "✍🏼",
     delay: 50,
   });
+}
+
+function generatePositivity(event) {
+  event.preventDefault();
+
+  instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "4a7bf3d9cod778ee0bbe6f174ftf4fa8";
+  let prompt = `Generate a positive ${instructionsInput.value}`;
+  let context =
+    "You are a supportive and inspirational AI Assistant. Based on the option submitted by the user, please generate a positive mantra, quote or words of affirmation. Make sure to follow user instructions. Never repeat the answer twice. Please behave. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating positivity");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayInspiration);
 }
 
 let formElement = document.querySelector("#positivity-generator-form");
