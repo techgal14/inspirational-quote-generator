07884ad182180c4ff1e8157fab5a167261ddad91
techgal14 <189483818+techgal14@users.noreply.github.com>
2025-03-04 23:15:54 -0800

    JavaScript
    - create function for text input
    - add typewriterJS
    - update CSS and HTML

--- src/interact.js
+++ src/interact.js
@@ -1,1 +1,13 @@
+function generatePositivity(event) {
+  event.preventDefault();
 
+  new Typewriter("#inspiration", {
+    strings: "whatever positive thing is generated",
+    autoStart: true,
+    cursor: "✍🏼",
+    delay: 50,
+  });
+}
+
+let formElement = document.querySelector("#positivity-generator-form");
+formElement.addEventListener("submit", generatePositivity);
